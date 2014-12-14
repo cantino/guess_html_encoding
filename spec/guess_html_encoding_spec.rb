efeec3b24452cf47cb2b58c61a7932bd6c490c90
Andrew Cantino <cantino@gmail.com>
2014-12-13 16:44:34 -0800

    Convert specs to RSpec 3.1.7 syntax with Transpec
    This conversion is done by Transpec 3.0.1 with the following command:
        transpec
    
    * 35 conversions
        from: obj.should
          to: expect(obj).to
    
    * 21 conversions
        from: == expected
          to: eq(expected)
    
    * 7 conversions
        from: lambda { }.should_not
          to: expect { }.not_to
    
    * 1 conversion
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/guess_html_encoding_spec.rb
+++ spec/guess_html_encoding_spec.rb
@@ -6,59 +6,59 @@ describe "GuessHtmlEncoding" do
     it "can use headers" do
       guess = GuessHtmlEncoding.guess("<html><body><div>hi!</div></body></html>",
                                       "Hello: world\nContent-Type: text/html; charset=LATIN1\nFoo: bar")
-      guess.should == "ISO-8859-1"
+      expect(guess).to eq("ISO-8859-1")
     end
 
     it "accepts headers as a hash as well" do
       guess = GuessHtmlEncoding.guess("<html><body><div>hi!</div></body></html>",
           {"Hello" => "world", "Content-Type" => "text/html; charset=LATIN1", "Foo" => "bar"})
-      guess.should == "ISO-8859-1"
+      expect(guess).to eq("ISO-8859-1")
     end
 
     it "accepts meta tags" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=LATIN1"></head><body><div>hi!</div></body></html>')
-      guess.should == "ISO-8859-1"
+      expect(guess).to eq("ISO-8859-1")
     end
 
     it "works okay when there is a semi-colon after the encoding with headers" do
       guess = GuessHtmlEncoding.guess("<html><body><div>hi!</div></body></html>",
                                       "Hello: world\nContent-Type: text/html; charset=utf-8;\nFoo: bar")
-      guess.should == "UTF-8"
+      expect(guess).to eq("UTF-8")
     end
 
     it "works okay when there is a semi-colon after the encoding with meta-tags" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8;"></head><body><div>hi!</div></body></html>')
-      guess.should == "UTF-8"
+      expect(guess).to eq("UTF-8")
     end
 
     it "converts UTF8 to UTF-8" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=utf8;"></head><body><div>hi!</div></body></html>')
-      guess.should == "UTF-8"
+      expect(guess).to eq("UTF-8")
     end
 
     it "converts CP-1251 to CP1251" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=cp-1251;"></head><body><div>hi!</div></body></html>')
-      guess.should == "CP1251"
+      expect(guess).to eq("CP1251")
     end
 
     it "skips the header content type if it's invalid" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=utf8;"></head><body><div>hi!</div></body></html>',
                                       "Hello: world\nContent-Type: text/html; charset=RU;\nFoo: bar")
-      guess.should == "UTF-8"
+      expect(guess).to eq("UTF-8")
     end
 
     it "translates WIN1251 to WINDOWS-1250" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=WIN1251;"></head><body><div>hi!</div></body></html>')
-      guess.should == "WINDOWS-1250"
+      expect(guess).to eq("WINDOWS-1250")
     end
 
     it "translates GB2312 to GB18030" do
       guess = GuessHtmlEncoding.guess('<html><head><meta http-equiv="content-type" content="text/html; charset=GB2312;"></head><body><div>hi!</div></body></html>')
-      guess.should == "GB18030"
+      expect(guess).to eq("GB18030")
     end
     
     it "should not raise an exception if data is nil" do
-      lambda { GuessHtmlEncoding.guess(nil) }.should_not raise_error
+      expect { GuessHtmlEncoding.guess(nil) }.not_to raise_error
     end
   end
 
@@ -66,96 +66,96 @@ describe "GuessHtmlEncoding" do
     it "should work on correctly encoded pages" do
       data = "<html><head><meta http-equiv='content-type' content='text/html; charset=utf8;'></head><body><div>hi!♥</div></body></html>"
       data.force_encoding("ASCII-8BIT")
-      data.should be_valid_encoding # everything is valid in binary
+      expect(data).to be_valid_encoding # everything is valid in binary
 
-      GuessHtmlEncoding.guess(data).should == "UTF-8" # because the page says so!
-      data.force_encoding("UTF-8").should be_valid_encoding # because it really is utf-8
+      expect(GuessHtmlEncoding.guess(data)).to eq("UTF-8") # because the page says so!
+      expect(data.force_encoding("UTF-8")).to be_valid_encoding # because it really is utf-8
 
       encoded = GuessHtmlEncoding.encode(data)
-      encoded.encoding.to_s.should == "UTF-8"
-      encoded.should be_valid_encoding
+      expect(encoded.encoding.to_s).to eq("UTF-8")
+      expect(encoded).to be_valid_encoding
     end
 
     it "should work on incorrectly encoded pages" do
       data = "<html><head><meta http-equiv='content-type' content='text/html; charset=utf8;'></head><body><div>hi!\xc2</div></body></html>"
       data.force_encoding("ASCII-8BIT")
-      data.should be_valid_encoding # everything is valid in binary
+      expect(data).to be_valid_encoding # everything is valid in binary
 
-      GuessHtmlEncoding.guess(data).should == "UTF-8" # because the page says so!
-      data.force_encoding("UTF-8").should_not be_valid_encoding # because of the bad byte sequence \xc2 which is not valid UTF-8
+      expect(GuessHtmlEncoding.guess(data)).to eq("UTF-8") # because the page says so!
+      expect(data.force_encoding("UTF-8")).not_to be_valid_encoding # because of the bad byte sequence \xc2 which is not valid UTF-8
 
       encoded = GuessHtmlEncoding.encode(data)
-      encoded.encoding.to_s.should == "UTF-8"
-      encoded.should be_valid_encoding
+      expect(encoded.encoding.to_s).to eq("UTF-8")
+      expect(encoded).to be_valid_encoding
     end
 
     it "should work on pages encoded with an unknown encoding by forcing them to utf8" do
       data = "<html><head><meta http-equiv='content-type' content='text/html; charset=x-mac-roman;'></head><body><div>hi!</div></body></html>"
       data.force_encoding("ASCII-8BIT")
-      data.should be_valid_encoding # everything is valid in binary
+      expect(data).to be_valid_encoding # everything is valid in binary
 
-      GuessHtmlEncoding.guess(data).should == "X-MAC-ROMAN" # because the page says so!
+      expect(GuessHtmlEncoding.guess(data)).to eq("X-MAC-ROMAN") # because the page says so!
 
       encoded = GuessHtmlEncoding.encode(data)
-      encoded.encoding.to_s.should == "UTF-8"
-      encoded.should be_valid_encoding
+      expect(encoded.encoding.to_s).to eq("UTF-8")
+      expect(encoded).to be_valid_encoding
 
-      data.encoding.to_s.should == "ASCII-8BIT"
+      expect(data.encoding.to_s).to eq("ASCII-8BIT")
     end
 
     it "should not raise an exception if data is nil" do
-      lambda { GuessHtmlEncoding.encode(nil) }.should_not raise_error
+      expect { GuessHtmlEncoding.encode(nil) }.not_to raise_error
     end
 
     it "should work on GB18030 (and translate GB2312 into GB18030)" do
       data = File.read(File.join(File.dirname(__FILE__), "fixtures/gb18030.html"), :encoding => "binary")
-      GuessHtmlEncoding.encoding_loaded?("GB18030").should be_truthy
-      GuessHtmlEncoding.guess(data).should == "GB18030"
-      GuessHtmlEncoding.encode(data).encoding.to_s.should == "GB18030"
+      expect(GuessHtmlEncoding.encoding_loaded?("GB18030")).to be_truthy
+      expect(GuessHtmlEncoding.guess(data)).to eq("GB18030")
+      expect(GuessHtmlEncoding.encode(data).encoding.to_s).to eq("GB18030")
     end
   end
 
   describe "#encoding_loaded?" do
     it 'returns true for all loaded encodings' do
       (Encoding.name_list - ["internal"]).each do |name|
-        GuessHtmlEncoding.encoding_loaded?(name).should be_truthy
-        lambda { Encoding.find(name) }.should_not raise_error
+        expect(GuessHtmlEncoding.encoding_loaded?(name)).to be_truthy
+        expect { Encoding.find(name) }.not_to raise_error
       end
     end
 
     it 'returns true for uppercase encodings' do
-      GuessHtmlEncoding.encoding_loaded?("WINDOWS-1250").should be_truthy
-      lambda { Encoding.find("WINDOWS-1250") }.should_not raise_error
+      expect(GuessHtmlEncoding.encoding_loaded?("WINDOWS-1250")).to be_truthy
+      expect { Encoding.find("WINDOWS-1250") }.not_to raise_error
     end
 
     it 'returns true for lowercase encodings' do
-      GuessHtmlEncoding.encoding_loaded?("windows-1250").should be_truthy
-      lambda { Encoding.find("windows-1250") }.should_not raise_error
+      expect(GuessHtmlEncoding.encoding_loaded?("windows-1250")).to be_truthy
+      expect { Encoding.find("windows-1250") }.not_to raise_error
     end
 
     it 'returns true for encoding aliases' do
       Encoding.aliases.keys.each do |key|
-        GuessHtmlEncoding.encoding_loaded?(key).should be_truthy
-        GuessHtmlEncoding.encoding_loaded?(key.upcase).should be_truthy
-        lambda { Encoding.find(key) }.should_not raise_error
-        lambda { Encoding.find(key.upcase) }.should_not raise_error
+        expect(GuessHtmlEncoding.encoding_loaded?(key)).to be_truthy
+        expect(GuessHtmlEncoding.encoding_loaded?(key.upcase)).to be_truthy
+        expect { Encoding.find(key) }.not_to raise_error
+        expect { Encoding.find(key.upcase) }.not_to raise_error
       end
     end
 
     it 'returns false for irregular or unloaded encoding' do
-      GuessHtmlEncoding.encoding_loaded?('_WHY').should be_falsy
+      expect(GuessHtmlEncoding.encoding_loaded?('_WHY')).to be_falsy
     end
 
     it "accepts a simple meta tag" do
       # Like http://www.taobao.com
       guess = GuessHtmlEncoding.guess('<html><head><meta charset="gbk" /></head><body><div>hi!</div></body></html>')
-      guess.should == "GBK"
+      expect(guess).to eq("GBK")
     end
 
     it "works as well when there is no double quotation marks with http-equiv in meta-tags" do
       # Like http://www.frozentux.net/iptables-tutorial/cn/iptables-tutorial-cn-1.1.19.html
       guess = GuessHtmlEncoding.guess('<html><head><META http-equiv=Content-Type content="text/html; charset=utf-8"></head><body><div>hi!</div></body></html>')
-      guess.should == "UTF-8"
+      expect(guess).to eq("UTF-8")
     end
   end
 end
